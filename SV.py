561d01b64f0078e162cf7beef7f4dd30ae750a8d
PimonovDaniil <PimonovDaniil@users.noreply.github.com>
2022-05-31 20:01:47 +0700

    фигуры пересекаются

--- SV.py
+++ SV.py
@@ -3,6 +3,8 @@ import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
 import math
+
+
 # https://habr.com/ru/post/265321/
 
 def matOzhid_dispers(x, y):
@@ -14,12 +16,19 @@ def matOzhid_dispers(x, y):
     dispers = m_circumflex_2 - (mat_ozhid ** 2)
     return [mat_ozhid, dispers]
 
+
 def Bernoulli(p):
     boundary = (1 - p)
     if random.random() > boundary:
         return 1
     else:
         return 0
+
+
+'''
+По определению случайная величина с биномиальным распределением — 
+это сумма n случайных величин с распределением Бернулли:
+'''
 
 
 # СВ с биномиальным распределением
@@ -34,13 +43,13 @@ def BinomialBernoulli(p, n):
 
 # Сам полином
 def Pn(m, p, n):
-    c = math.gamma(n+1) / (math.gamma(m+1) * math.gamma(n - m+1))
+    c = math.gamma(n + 1) / (math.gamma(m + 1) * math.gamma(n - m + 1))
     return c * (p ** m) * ((1 - p) ** (n - m))
 
 
 polinom = []
 for i in range(150):
-    polinom.append(Pn(i/10, 0.35, 15))
+    polinom.append(Pn(i / 10, 0.35, 15))
 
 res = [0] * 15
 y = list(range(15))
@@ -64,9 +73,9 @@ for i in range(len(y2)):
 
 m1, dispers1 = matOzhid_dispers(y2, polinom)
 m2, dispers2 = matOzhid_dispers(y, res)
-print("Мат ожидание биноминального распределения: "+str(m1))
-print("Дисперсия биноминального распределения: "+str(dispers1))
-print("Мат ожидание СВ биноминального распределения: "+str(m2))
-print("Дисперсия СВ биноминального распределения: "+str(dispers2))
+print("Мат ожидание биноминального распределения: " + str(m1))
+print("Дисперсия биноминального распределения: " + str(dispers1))
+print("Мат ожидание СВ биноминального распределения: " + str(m2))
+print("Дисперсия СВ биноминального распределения: " + str(dispers2))
 plt.plot(y2, polinom)
 matplotlib.pyplot.show()
--- figure.py
+++ figure.py
@@ -1,6 +1,7 @@
 import math
 import random
 
+
 def cube(x, y, z):
     size = 10
     # Если точка находится в кубе ()
@@ -21,21 +22,38 @@ def cilindr(x, y, z):
         return False
 
 
-print("Объём фигур аналитически: ", (10 * 10 * 10) + (math.pi * 9 * 10))
-print("Объём пространства в пределах которого генерируем случайные точки: ",20*20*20)
-
-pointIn = 0
-pointOut = 0
-for i in range(10000000):
-    x = random.random() * 20
-    y = random.random() * 20
-    z = random.random() * 20
-    if cube(x, y, z) or cilindr(x, y, z):
-        pointIn += 1
+def cilindrVkube(x, y, z):
+    cubeSize = 10  # ДОЛЖЕН СОВПАДАТЬ С РАЗМЕРОМ КУБА
+    radCilindr = 3
+    hCilindr = 5
+    rast = math.sqrt((x - (cubeSize / 2)) ** 2 + (y - (cubeSize / 2)) ** 2)
+    if rast <= radCilindr and cubeSize <= z <= cubeSize + hCilindr:
+        return True
     else:
-        pointOut += 1
-print("Кол-во точек попало в пределы фигур: ", pointIn)
-print("Кол-во точек не попало в пределы фигур: ", pointOut)
-print("Объём посчитанный методом монте-карла", ((20*20*20)*pointIn)/(pointOut+pointIn))
+        return False
 
 
+def monteKarlik(cube, cilindr, vAnal):
+    print("Объём фигур аналитически: ", vAnal)
+    print("Объём пространства в пределах которого генерируем случайные точки: ", 20 * 20 * 20)
+
+    pointIn = 0
+    pointOut = 0
+    for i in range(100000):
+        x = random.random() * 20
+        y = random.random() * 20
+        z = random.random() * 20
+        if cube(x, y, z) or cilindr(x, y, z):
+            pointIn += 1
+        else:
+            pointOut += 1
+    print("Кол-во точек попало в пределы фигур: ", pointIn)
+    print("Кол-во точек не попало в пределы фигур: ", pointOut)
+    print("Объём посчитанный методом монте-карла", ((20 * 20 * 20) * pointIn) / (pointOut + pointIn))
+    print("Разница между аналитическим и Монте-Карло", abs((((20 * 20 * 20) * pointIn) / (pointOut + pointIn)) - vAnal))
+
+
+print("Куб со стороной 10, цилинтр с радиусом 3 и высотой 10")
+monteKarlik(cube, cilindr, (10 * 10 * 10) + (math.pi * 9 * 10))
+print("\nКуб со стороной 10, цилинтр с радиусом 3 и высотой 10 но на половину в кубе")
+monteKarlik(cube, cilindrVkube, (10 * 10 * 10) + (math.pi * 9 * 5))
